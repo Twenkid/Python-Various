1b64952ce857bb6b8f9f0deb8825c362ec77b0b6
Todor Arnaudov <twenkid@gmail.com>
2020-12-08 22:42:49 +0200

    Update t9_zrim1.py

--- Exercise/t9_zrim1.py
+++ Exercise/t9_zrim1.py
@@ -1,10 +1,12 @@
-#Zrim, Зрим, ... (друго име трябва...)
-#Тодор Арнаудов - Тош, Разумир, Razumir, Twenkid, Tosh... 22-3-2016
-#t9_zrim1.py
+# Zrim, Зрим, ... (друго име трябва...)
+# Explanation of walking directories and file with the Todor's cognitive notation "Zrim" (Зрим)
+# Author: Todor Arnaudov - Тодор Арнаудов - Тош, Разумир, Razumir, Twenkid, Tosh... 22-3-2016
+# t9_zrim1.py
 
 def walkExample(path):		#  @  \|/ ({K}!({пП}) ... -*- обПмт.&[1] ...  (,нч)   ... 
 	import os				#  @  -*-{К-К}("os") 
-	for root, dirs, files in os.walk(path, topdown=False):	# Обх.свеждане.{бр[.].вт=3(&[.]1 = "root", &[.]2 = "dirs", &[.]3="files"; &[.].вн = (!="os.walk", !.#=(&[.]1 = "path" = ОбПмт.&[1], &[.]2 = "topdown=False".свеждане)) (,нч)
+	for root, dirs, files in os.walk(path, topdown=False):
+	# Обх.свеждане.{бр[.].вт=3(&[.]1 = "root", &[.]2 = "dirs", &[.]3="files"; &[.].вн = (!="os.walk", !.#=(&[.]1 = "path" = ОбПмт.&[1], &[.]2 = "topdown=False".свеждане)) (,нч)
 	# &[.]1 = "root" -*- пмт.&[1], ...
 	
 	for name in files:		# (Обх.свеждане.{бр[.].вт=1(&[.]1 = "name"; &[.].вн = "files" (,нч)
