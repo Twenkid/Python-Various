518e10af02a7549245c9b4f913f7795ad2f7c139
Todor Arnaudov <twenkid@gmail.com>
2020-12-15 04:56:33 +0200

    Add files via upload

---
+++ Exercise/Generate_HTML/t43.py
@@ -0,0 +1,13 @@
+#t43.py
+#25-5-2015
+
+for j in range(1,30): print("<a href=\"#"+str(j)+"\">");
+for j in range(1,30): print("<a name=\""+str(j)+"\">");
+
+
+#f1 = open("listEvents.txt", "w+")
+#f1.write(v2)
+#f1.close()
+
+
+
